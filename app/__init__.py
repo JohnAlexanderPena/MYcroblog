febcb3ccf06a131838c95b6daf9a8166b59c4799
John <jpena91@gmail.com>
2020-01-22 15:48:24 -0500

    add login, register, and redirect functions

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from app import routes, models
 
 #set to the name of the module being used
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app) # represents the database
-migrate = Migrate(app, db) 
-
-from app import routes, models
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
