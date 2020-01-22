66a140f0e5e3de54e1167af19cf486d68537e788
John <jpena91@gmail.com>
2020-01-22 13:23:39 -0500

    add post and user models with shell context processor

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 #set to the name of the module being used
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app) # represents the database
+migrate = Migrate(app, db) 
 
-from app import routes
+from app import routes, models
